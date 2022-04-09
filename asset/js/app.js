3d0ba8c38d25c4c36931b555d74d935d9a7d3d0c
jhon-Min <minnyisay@gmail.com>
2022-04-10 01:48:20 +0630

    complete emp page

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,3 +4,17 @@ $("#single-select-clear-field").select2({
   dropdownCssClass: "select2--small",
   //   allowClear: true,
 });
+
+let myDropzone = new Dropzone("#my-awesome-dropzone");
+myDropzone.on("addedfile", (file) => {
+  console.log(`File added: ${file.name}`);
+});
+
+let dropText = document.querySelector(".dropzone .dz-message .dz-button");
+
+dropText.innerHTML = `
+  <div>
+  <i class="fa-solid fa-file-image file-upload-icon mb-3"></i>
+  <h5 class="mb-0">Drop files here or click to upload.</h5>
+  </div>
+`;
